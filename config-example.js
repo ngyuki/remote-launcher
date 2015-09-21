9750a2e5807536053b40279bee0a6f64c07c4409
ngyuki <ngyuki.jp@gmail.com>
2015-09-21 19:52:18 +0900

    config example

---
+++ config-example.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    replacePaths: [
+        { pattern: '^/home/your/Dropbox(/|$)',  replace: 'C:\\Users\\your\\Dropbox$1' },
+        { pattern: '^/home/your/devel(/|$)',    replace: 'C:\\Users\\your\\devel$1' },
+        { pattern: '^/home/your/download(/|$)', replace: 'C:\\Users\\your\\download$1' },
+        { pattern: '^/home/your(/|$)',          replace: '\\\\linux-server\\your$1' },
+    ],
+
+    allowHosts: [
+        '192.168.1.234',
+    ]
+}

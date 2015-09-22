1564e0294792e80dbc8e5a29720ed702d8199edf
ngyuki <ngyuki.jp@gmail.com>
2015-09-22 11:16:28 +0900

    change sublime bin, env to config

--- config-example.js
+++ config-example.js
@@ -1,5 +1,7 @@
 module.exports = {
 
+    sublime_bin: "C:\\Program Files\\sublime_text\\sublime_text.exe",
+
     replacePaths: [
         { pattern: '^/home/your/Dropbox(/|$)',  replace: 'C:\\Users\\your\\Dropbox$1' },
         { pattern: '^/home/your/devel(/|$)',    replace: 'C:\\Users\\your\\devel$1' },
--- src/work.js
+++ src/work.js
@@ -1,11 +1,12 @@
 var exec = require('child_process').exec;
 var fixargs = require('./fixargs');
 var escapeargs = require('./escapeargs');
+var sublime_bin = require('../config').sublime_bin;
 
 module.exports = function(args){
     //console.log(args);
 
-    var cmds = [process.env.SUBLIME_BIN].concat(fixargs(args));
+    var cmds = [sublime_bin].concat(fixargs(args));
     var commandline = escapeargs(cmds).join(" ");
 
     console.log(commandline);

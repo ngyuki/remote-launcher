61f7f98dc87b5bcf207145fa7ddde52705b1c68f
ngyuki <ngyuki.jp@gmail.com>
2015-09-22 11:30:58 +0900

    Changed process creation method from exec to spawn

--- src/work.js
+++ src/work.js
@@ -1,14 +1,11 @@
-var exec = require('child_process').exec;
+var util = require('util');
+var spawn = require('child_process').spawn;
 var fixargs = require('./fixargs');
 var escapeargs = require('./escapeargs');
 var sublime_bin = require('../config').sublime_bin;
 
 module.exports = function(args){
-    //console.log(args);
-
-    var cmds = [sublime_bin].concat(fixargs(args));
-    var commandline = escapeargs(cmds).join(" ");
-
-    console.log(commandline);
-    exec(commandline);
+    args = fixargs(args);
+    console.log(util.format('spawn ... %s %s', sublime_bin, JSON.stringify(args)));
+    spawn(sublime_bin, args, {stdio: 'ignore', detached: true});
 }

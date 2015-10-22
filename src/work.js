a2c57de1bfcbd5d500a2b2365c2737e88607a973
ngyuki <ngyuki.jp@gmail.com>
2015-10-22 13:43:07 +0900

    ignore network drive for cwd

--- src/work.js
+++ src/work.js
@@ -1,18 +1,35 @@
 var util = require('util');
+var path = require('path');
 var spawn = require('child_process').spawn;
+
+function isNetworkDrive(p)
+{
+    if (path.normalize(p).substr(0, 2) == '\\\\') {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 module.exports = function(data){
     var command = data.command;
     var args = data.args;
     var cwd = data.cwd;
 
-    console.log(util.format('spawn ... (%s) %s %s', cwd, command, JSON.stringify(args)));
-
-    var proc = spawn('cmd.exe', ['/S', '/C', command].concat(args), {
+    var opts = {
         stdio: 'ignore',
         detached: true,
-        cwd: cwd
-    });
+    };
+
+    if (isNetworkDrive(cwd)) {
+        cwd = null;
+    } else {
+        opts.cwd = cwd;
+    }
+
+    console.log(util.format('spawn ... (%s) %s %s', cwd, command, JSON.stringify(args)));
+
+    var proc = spawn('cmd.exe', ['/S', '/C', command].concat(args), opts);
 
     proc.on('error', function(err){
         console.log(util.format('error ... ' + err));

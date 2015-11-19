b7e8b4e918d0795df7bb9c21163ad1e7006af2c4
ngyuki <ngyuki.jp@gmail.com>
2015-11-19 09:18:43 +0900

    Fix argument parse

--- bin/temoto.js
+++ bin/temoto.js
@@ -43,9 +43,26 @@ function exec()
     require('../src/client')(args);
 }
 
-var opt = process.argv[2];
+function parseArguments()
+{
+    var path = require('path');
+    var args;
 
-if ((opt == null) || (opt[0] != '-')) {
+    if (path.basename(process.argv[1], '.js') === path.basename(__filename, '.js')) {
+        args = process.argv.slice(2);
+    } else {
+        args = process.argv.slice(1);
+        args[0] = path.basename(args[0]);
+    }
+
+    return args;
+}
+
+var args = parseArguments();
+
+var opt = args[0];
+
+if (opt[0] != '-') {
     exec();
     return;
 }

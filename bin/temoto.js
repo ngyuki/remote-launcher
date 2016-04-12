6c44ece28075c9ebfbafed18980eebd9cdb9a9cb
ngyuki <ngyuki.jp@gmail.com>
2016-04-12 23:21:53 +0900

    Fix cli

--- bin/temoto.js
+++ bin/temoto.js
@@ -28,18 +28,8 @@ function local()
     require('../src/replacePath.js').writeLocal(process.cwd());
 }
 
-function exec()
+function exec(args)
 {
-    var path = require('path');
-    var args;
-
-    if (path.basename(process.argv[1], '.js') === path.basename(__filename, '.js')) {
-        args = process.argv.slice(2);
-    } else {
-        args = process.argv.slice(1);
-        args[0] = path.basename(args[0]);
-    }
-
     require('../src/client')(args);
 }
 
@@ -73,6 +63,7 @@ function parseArguments()
     if (path.basename(process.argv[1], '.js') === path.basename(__filename, '.js')) {
         args = process.argv.slice(2);
     } else {
+        // for symlink
         args = process.argv.slice(1);
         args[0] = path.basename(args[0]);
     }
@@ -80,37 +71,38 @@ function parseArguments()
     return args;
 }
 
-var args = parseArguments();
+(function(){
+    var args = parseArguments();
+    var opt = args[0];
 
-var opt = args[0];
-
-if (opt == null) {
-    usage();
-    return;
-}
-
-if (opt[0] != '-') {
-    exec();
-    return;
-}
-
-switch (opt) {
-    case '--server':
-    case '-s':
-        server();
-        return;
-
-    case '--local':
-    case '-l':
-        local();
-        return;
-
-    case '--help':
-    case '-h':
-        help();
-        return;
-
-    default:
+    if (opt == null) {
         usage();
         return;
-}
+    }
+
+    if (opt[0] != '-') {
+        exec(args);
+        return;
+    }
+
+    switch (opt) {
+        case '--server':
+        case '-s':
+            server();
+            return;
+
+        case '--local':
+        case '-l':
+            local();
+            return;
+
+        case '--help':
+        case '-h':
+            help();
+            return;
+
+        default:
+            usage();
+            return;
+    }
+}())
